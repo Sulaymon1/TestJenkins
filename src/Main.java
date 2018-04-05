e73280e773c110a6b26eae9d81b2bf73669703f6
Sulaymon <demirel6777@gmail.com>
2018-04-05 23:27:45 +0300

    Test for Jenkins automation

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Date 05.04.2018
+ *
+ * @author Hursanov Sulaymon
+ * @version v1.0
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, JENKINS from Github");
+    }
+}
